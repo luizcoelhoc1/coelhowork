c96676562a61a5036418a6299a10410086229cf2
luizcoelhoc1 <luizcoelhoc1@gmail.com>
2023-09-25 21:51:52 -0300

    feat(RestController): CROS controll

--- core/Controller/RestController.php
+++ core/Controller/RestController.php
@@ -10,6 +10,18 @@ abstract class RestController {
         echo json_encode($json);
     }
     
+    public function loads() {
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+        header('Content-Type: application/json; charset=utf-8');
+    }
+    
     public function output_error($exception) {
         $code = $exception->getCode();
         if (is_http_code($code)) {
